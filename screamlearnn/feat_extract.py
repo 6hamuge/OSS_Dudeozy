0f21b064f7dc40530028dd3e556825280e0b8a8d
Haydnkr <165639764+Haydnkr@users.noreply.github.com>
2024-06-19 13:20:01 +0900

    Update feat_extract.py

--- screamlearnn/feat_extract.py
+++ screamlearnn/feat_extract.py
@@ -1,17 +1,9 @@
-#!/usr/bin/env python
-# coding= UTF-8
-#
-# Author: Fing
-# Date  : 2017-12-03
-#
-
 import code
 import glob
 import os
 import librosa
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.pyplot import specgram
 import soundfile as sf
 import sounddevice as sd
 import queue
@@ -35,60 +27,49 @@ def extract_feature(file_name=None):
     if X.ndim > 1: X = X[:,0]
     X = X.T
 
-    # short term fourier transform
     stft = np.abs(librosa.stft(X))
-
-    # mfcc (mel-frequency cepstrum)
     mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T,axis=0)
-
-    # chroma
     chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
-
-    # melspectrogram
     mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
-
-    # spectral contrast
     contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
-
     tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X), sr=sample_rate).T,axis=0)
     return mfccs,chroma,mel,contrast,tonnetz
 
-def parse_audio_files(parent_dir,file_ext='*.ogg'):
-    sub_dirs = os.listdir(parent_dir)
-    sub_dirs.sort()
-    features, labels = np.empty((0,193)), np.empty(0)
-    for label, sub_dir in enumerate(sub_dirs):
-        if os.path.isdir(os.path.join(parent_dir, sub_dir)):
-            for fn in glob.glob(os.path.join(parent_dir, sub_dir, file_ext)):
-                try: mfccs, chroma, mel, contrast,tonnetz = extract_feature(fn)
-                except Exception as e:
-                    print("[Error] extract feature error in %s. %s" % (fn,e))
-                    continue
-                ext_features = np.hstack([mfccs,chroma,mel,contrast,tonnetz])
-                features = np.vstack([features,ext_features])
-                # labels = np.append(labels, fn.split('/')[1])
-                labels = np.append(labels, label)
-            print("extract %s features done" % (sub_dir))
-    return np.array(features), np.array(labels, dtype = np.int)
+#def parse_audio_files(parent_dir, file_ext='*.ogg'):
+#    sub_dirs = os.listdir(parent_dir)
+#    sub_dirs.sort()
+#    features, labels = np.empty((0,193)), np.empty(0)
+#    for label, sub_dir in enumerate(sub_dirs):
+#        if os.path.isdir(os.path.join(parent_dir, sub_dir)):
+#            for fn in glob.glob(os.path.join(parent_dir, sub_dir, file_ext)):
+#                try: mfccs, chroma, mel, contrast,tonnetz = extract_feature(fn)
+#                except Exception as e:
+#                    print("[Error] extract feature error in %s. %s" % (fn,e))
+#                    continue
+#                ext_features = np.hstack([mfccs,chroma,mel,contrast,tonnetz])
+#                features = np.vstack([features,ext_features])
+#                labels = np.append(labels, label)
+#            print("extract %s features done" % (sub_dir))
+#    return np.array(features), np.array(labels, dtype = np.int)
 
-def parse_predict_files(parent_dir,file_ext='*.ogg'):
-    features = np.empty((0,193))
-    filenames = []
-    for fn in glob.glob(os.path.join(parent_dir, file_ext)):
-        mfccs, chroma, mel, contrast,tonnetz = extract_feature(fn)
-        ext_features = np.hstack([mfccs,chroma,mel,contrast,tonnetz])
-        features = np.vstack([features,ext_features])
-        filenames.append(fn)
-        print("extract %s features done" % fn)
-    return np.array(features), np.array(filenames)
+def parse_audio_files(parent_dir, file_ext='*.ogg'):
+    scream_dir = os.path.join(parent_dir, 'scream')
+    non_scream_dir = os.path.join(parent_dir, 'non_scream')
+
+    features_scream, labels_scream = parse_audio_files_in_dir(scream_dir, file_ext)
+    features_non_scream, labels_non_scream = parse_audio_files_in_dir(non_scream_dir, file_ext)
+
+    features = np.vstack([features_scream, features_non_scream])
+    labels = np.hstack([labels_scream, labels_non_scream])
+
+    return features, labels
+
 
 def main():
-    # Get features and labels
     features, labels = parse_audio_files('data')
     np.save('feat.npy', features)
     np.save('label.npy', labels)
 
-    # Predict new
     features, filenames = parse_predict_files('predict')
     np.save('predict_feat.npy', features)
     np.save('predict_filenames.npy', filenames)
